b4ba9c2db84e726e3fea1aa5c570d3a99e3cb181
yinruihan <yinrh2023@shanghaitech.edu.cn>
2024-03-07 18:09:36 +0800

    changes in npc

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,39 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include <Vlight.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME *dut);
+
+void single_cycle()
+{
+	dut.clk = 0;
+	dut.eval();
+	dut.clk = 1;
+	dut.eval();
+}
+
+void reset(int n)
+{
+	dut.rst = 1;
+	while (n-- > 0)
+		single_cycle();
+	dut.rst = 0;
+}
+
+int main()
+{
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+	reset(10);
+
+	while (1)
+	{
+		nvboard_update();
+		single_cycle();
+	}
+	
+
+	return 0;
 }
